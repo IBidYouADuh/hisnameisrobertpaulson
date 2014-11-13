451aa0fd8b6780ac3212de5e1bb031e98fdb774f
sdowding <sad343@nyu.edu>
2014-11-13 01:27:10 +0000

    updated postSubmit.html postSubmit.js posts.js router.js

--- client/views/posts/postSubmit.js
+++ client/views/posts/postSubmit.js
@@ -1,27 +1,21 @@
 Template.postSubmit.events({
-  'submit form': function(e){
+  'submit form': function(e) {
     e.preventDefault();
     
     var post = {
       title: $(e.target).find('[name=post-title]').val(),
       url: $(e.target).find('[name=post-url]').val(),
-      description: $(e.target).find('[name=post-description]').val(),
+      description: $(e.target).find('[name=post-description]').val()
     }
     
-    post._id = Posts.insert(post);
-    
-    Router.go('postPage', post)
-    
-
     //post._id = Posts.insert(post);
     
     Meteor.call('post', post, function(error, id){
       if (error)
         return alert(error.reason);
       
-      Router.go('postPage',{_id:id});
+      Router.go('postPage', {_id:id});
       
-    }); 
+    });
   }
-   
 });
--- collections/posts.js
+++ collections/posts.js
@@ -1,13 +1,18 @@
 Posts = new Meteor.Collection('posts');
 
 Posts.allow({
-  insert: function(userId, doc){
-    return !! userId;
+  update: function(userId, post){
+    return ownsDocument(userId, post);
   },
-  update: function(userId, doc){
-    return !! userId;
+  remove: function(userId, post){
+    return ownsDocument(userId, post);
   }
+});
 
+Posts.deny({
+  update: function(userId, post, fieldNames){
+    return (_.without(fieldNames, 'title', 'url', 'description').length > 0)
+  }
 });
 
 Meteor.methods({
@@ -16,33 +21,30 @@ Meteor.methods({
     var user = Meteor.user(),
         duplicatePost = Posts.findOne({url: postAttributes.url});
     
-    //make sure user is logged in
+    // make sure user is logged in
     if (!user)
-      throw new Meteor.Error(401, "Uh-oh! You need to be logged in!");
+      throw new Meteor.Error(401, "Uh-oh! You need to be logged!");
     
-    //make sure postAttributes has a title and URL
+    // make sure that postAttributes has a title and URL
     if (!postAttributes.title)
       throw new Meteor.Error(422, "Uh-oh! You need a title!");
-    
-    if (!postAttributes.title)
+      
+    if (!postAttributes.url)
       throw new Meteor.Error(422, "Uh-oh! You need a URL!");
     
-    //check to make sure URL has not already been submitted
+    // check to make sure that the URL has not already been submitted
     if (postAttributes.url && duplicatePost) {
-      throw new Meteor.Error(302, "This link has already been posted.", duplicatePost._id);
+      throw new Meteor.Error(302, "This link has already been posted", duplicatePost._id);
     }
- 
     
+    // grab the right fields, and add new fields such as: username, timestamp
+    var post = _.extend(_.pick(postAttributes, 'url', 'title', 'description'), {
+      userId: user._id,
+      author: user.profile.name, // if using Facebook account, then username is not available. Use profile.name instead
+      submitted: new Date().getTime()
+    });
     
-    //grab the right fields, and add new fields such as: username, timestamp
-    var post =_.extend(_.pick(postAttributes, 'url', 'title', 'description'),
-        {userId: user._id,
-         author: user.username,
-         submitted: new Date(). getTime()
-  });
-  
-  
-  var postId = Posts.insert(post);
+    var postId = Posts.insert(post);
     
     return postId;
   }
